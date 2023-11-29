a72bf70caf180fb97183e576b02e5bdc76763637
Lucas Oliveira <lucaoxt@gmail.com>
2023-11-29 20:19:20 -0300

    crud implementation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,11 +23,41 @@ app.UseHttpsRedirection();
 
 app.MapGet("/tarefas", async (APITarefasContext db) => await db.Tarefas.ToListAsync());
 
+app.MapGet("/tarefas/concluida", async (APITarefasContext db) => await db.Tarefas.Where(t => t.IsConcluida).ToListAsync());
+
+app.MapGet("/tarefas/{id}", async (int id, APITarefasContext db) => await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+
 app.MapPost("/tarefas", async (Tarefa tarefa, APITarefasContext db) =>
 {
     db.Tarefas.Add(tarefa);
     await db.SaveChangesAsync();
     return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
+
+app.MapPut("/tarefas/{id}", async (int id, Tarefa inputTarefa, APITarefasContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+
+    if (tarefa is null) Results.NotFound();
+
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.IsConcluida = inputTarefa.IsConcluida;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/tarefas/{id}", async (int id, APITarefasContext db) => 
+{
+    if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+        return Results.Ok(tarefa);
+    }
+
+    return Results.NotFound();
 });
 
 app.Run();
