e71d91754ef46732957be2c99ac079f4ca9e11a7
Lucas Oliveira <lucaoxt@gmail.com>
2023-11-29 16:46:29 -0300

    get and post resources

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<APITarefasContext>(options => options.UseInMemoryDatabase("TarefasDB"));
 
 var app = builder.Build();
 
@@ -16,4 +20,28 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/tarefas", async (APITarefasContext db) => await db.Tarefas.ToListAsync());
+
+app.MapPost("/tarefas", async (Tarefa tarefa, APITarefasContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
+
 app.Run();
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class APITarefasContext : DbContext
+{
+    public APITarefasContext(DbContextOptions<APITarefasContext> options) : base(options)
+    {}
+
+    public DbSet<Tarefa>? Tarefas => Set<Tarefa>();
+}
